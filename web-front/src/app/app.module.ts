57ac1c2a41232f43ee04ff221a563473a461c498
Christian Torres <cctorres@asesoftware.com>
2021-04-23 00:58:30 -0500

    WEBFRONT-Add pop-up createClient.

--- web-front/src/app/app.module.ts
+++ web-front/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ClientComponent } from './client/client.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PopUpComponent } from './shared/pop-up/pop-up.component';
+import { FormClientComponent } from './form-client/form-client.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientComponent
+    ClientComponent,
+    PopUpComponent,
+    FormClientComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
